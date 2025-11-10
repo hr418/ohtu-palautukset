42129113b9a848bb6ac5b8cb8caee8f5710cc0a8
rasse201 <h.rasmus200@gmail.com>
2025-11-10 21:06:55 +0200

    Week 2 task 2

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,25 @@
+import requests
+from player import Player
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == "FIN":
+            players.append(player)
+
+    print("Players from FIN:")
+
+    for player in players:
+        print(
+            f"{player.name} team {player.team} goals {player.goals} assists {player.assists}"
+        )
+
+
+if __name__ == "__main__":
+    main()
