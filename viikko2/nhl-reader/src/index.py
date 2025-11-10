c87f6868b96ca1ca170d92a1071697fb4ddb6ebf
rasse201 <h.rasmus200@gmail.com>
2025-11-10 22:03:24 +0200

    Week 2 task 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,14 +1,77 @@
-from player import Player, PlayerReader, PlayerStats
+from rich.prompt import Prompt
+from rich.table import Table
+from rich.console import Console
+from player import PlayerReader, PlayerStats
 
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+
+    season = Prompt.ask(
+        "Season",
+        default="2024-25",
+        choices=[
+            "2018-19",
+            "2019-20",
+            "2020-21",
+            "2021-22",
+            "2022-23",
+            "2023-24",
+            "2024-25",
+            "2025-26",
+        ],
+    )
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
 
-    for player in players:
-        print(player)
+    while True:
+        nationality = Prompt.ask(
+            "Nationality",
+            choices=[
+                "USA",
+                "FIN",
+                "CAN",
+                "SWE",
+                "RUS",
+                "CZE",
+                "SLO",
+                "FRA",
+                "GBR",
+                "SVK",
+                "DEN",
+                "NED",
+                "AUT",
+                "BLR",
+                "GER",
+                "SUI",
+                "NOR",
+                "UZB",
+                "LAT",
+                "AUS",
+            ],
+        )
+
+        top_players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Season {season} players from {nationality}")
+        table.add_column("Released", justify="left", style="cyan")
+        table.add_column("teams", justify="left", style="magenta")
+        table.add_column("goals", justify="right", style="green")
+        table.add_column("assists", justify="right", style="green")
+        table.add_column("points", justify="right", style="green")
+
+        for player in top_players:
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.goals + player.assists),
+            )
+
+        console = Console()
+        console.print(table)
 
 
 if __name__ == "__main__":
