a03a9f6d4d23304d347105d95d5f5e9c7c49eca4
rasse201 <h.rasmus200@gmail.com>
2025-11-10 21:39:06 +0200

    Week 2 task 3

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,26 +1,14 @@
-import requests
-from player import Player
+from player import Player, PlayerReader, PlayerStats
 
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
-
-    players = []
-
-    for player_dict in response:
-        player = Player(player_dict)
-        if player.nationality == "FIN":
-            players.append(player)
-
-    players.sort(key=lambda p: p.goals + p.assists, reverse=True)
-
-    print("Players from FIN:")
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
-        print(
-            f"{player.name:20} {player.team:15} {player.goals} + {player.assists} = {player.goals + player.assists}"
-        )
+        print(player)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Player:
     def __init__(self, dict):
         self.name = dict["name"]
@@ -8,4 +11,23 @@ class Player:
         self.games = dict["games"]
 
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team:15} {self.goals} + {self.assists} = {self.goals + self.assists}"
+
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        return [Player(player_dict) for player_dict in response]
+
+
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.players = player_reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        filtered_players = [p for p in self.players if p.nationality == nationality]
+        filtered_players.sort(key=lambda p: p.goals + p.assists, reverse=True)
+        return filtered_players
